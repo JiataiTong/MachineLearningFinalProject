c0668a097fb40a7f222e5e1162ad3be7734b7b7f
Jiatai Tong <jt037@bucknell.edu>
2024-11-25 17:01:05 -0600

    minor changes over data_generator.py, avoid duplicating generating

--- data_generator.py
+++ data_generator.py
@@ -1,8 +1,12 @@
 from dataset.CSV_Generation import *
+import os
 
 sample_num_list = {10, 100, 1000, 10000}
 
 for sample_num in sample_num_list:
-    generate_csv_data(sample_num, f'training_{sample_num}.csv')
-    generate_csv_data(sample_num, f'validating_{sample_num}.csv')
-    generate_csv_data(sample_num, f'testing_{sample_num}.csv')
+    if not os.path.exists(f'training_{sample_num}.csv'):
+        generate_csv_data(sample_num, f'training_{sample_num}.csv')
+    if not os.path.exists(f'validating_{sample_num}.csv'):
+        generate_csv_data(sample_num, f'validating_{sample_num}.csv')
+    if not os.path.exists(f'testing_{sample_num}.csv'):
+        generate_csv_data(sample_num, f'testing_{sample_num}.csv')
