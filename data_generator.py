237381467c348b0fc7a21a6ad78b9a568550cd9b
Jiatai Tong <jt037@bucknell.edu>
2024-11-25 16:57:09 -0600

    dataset_generated

--- data_generator.py
+++ data_generator.py
@@ -1,6 +1,6 @@
 from dataset.CSV_Generation import *
 
-sample_num_list = {10, 100, 1000}
+sample_num_list = {10, 100, 1000, 10000}
 
 for sample_num in sample_num_list:
     generate_csv_data(sample_num, f'training_{sample_num}.csv')
